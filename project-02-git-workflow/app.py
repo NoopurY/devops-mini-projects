12ae43268480e1e9715349dc83696a52d701ad44
Noopur Yawalkar <ashwinagrawal400@gmail.com>
2026-06-09 21:56:49 +0530

    Base to-do app

--- project-02-git-workflow/app.py
+++ project-02-git-workflow/app.py
@@ -1,1 +1,23 @@
-print("Main application")
+tasks = []
+
+def add_task(task):
+    tasks.append(task)
+    print(f"Task '{task}' added.")
+
+def view_tasks():
+    if not tasks:
+        print("No tasks available.")
+    else:
+        print("Your Tasks:")
+        for i, task in enumerate(tasks, 1):
+            print(f"{i}. {task}")
+
+def delete_task(index):
+    if 0 < index <= len(tasks):
+        removed = tasks.pop(index - 1)
+        print(f"Removed task: {removed}")
+    else:
+        print("Invalid task number")
+
+if __name__ == "__main__":
+    print("Simple To-Do App")
